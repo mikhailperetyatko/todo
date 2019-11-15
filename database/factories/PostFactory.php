486576be01dae163a198b4bc2e6810fc4f342d79
Mikhail Peretyatko <mihanya@list.ru>
2019-11-15 17:02:57 +0100

    Bug fixed in PostFactory

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -8,7 +8,7 @@ use Faker\Generator as Faker;
 $factory->define(Post::class, function (Faker $faker) {
     return [
         'owner_id' => app('users')->random(),
-        'slug' => $faker->words(1, true),
+        'slug' => $faker->unique()->words(1, true),
         'title' => $faker->words(5, true),
         'description' => $faker->words(15, true),
         'body' => $faker->words(30, true),
