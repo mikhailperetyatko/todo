eaee56c56a8b6d1b2a6365c9b7990a798ed8de4d
Mikhail Peretyatko <mihanya@list.ru>
2019-12-05 16:01:20 +0100

    Deleted resources/views/posts/components/manage_depr/*

--- resources/views/posts/components/manage_depr/admin.blade.php
+++
@@ -1,6 +0,0 @@
-<a class="btn btn-outline-secondary btn-sm" href="/admin/posts/{{ $post->slug }}/edit">Изменить</a>
-<form class="mt-2 d-inline" method="post" action="/admin/posts/{{$post->slug}}">
-  {{ csrf_field() }}
-  {{ method_field('DELETE') }}
-  <button type="submit" class="btn btn-outline-danger btn-sm">Удалить</button>
-</form>
--- resources/views/posts/components/manage_depr/user.blade.php
+++
@@ -1,6 +0,0 @@
-<a class="btn btn-outline-secondary btn-sm" href="/posts/{{ $post->slug }}/edit">Изменить</a>
-<form class="mt-2 d-inline" method="post" action="/posts/{{$post->slug}}">
-  {{ csrf_field() }}
-  {{ method_field('DELETE') }}
-  <button type="submit" class="btn btn-outline-danger btn-sm">Удалить</button>
-</form>
