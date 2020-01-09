4d873210a8c9203f3eed4927112881bb980db777
Mikhail Peretyatko <mihanya@list.ru>
2020-01-09 13:43:31 +0100

    Updated app/Jobs/StatisticsReport

--- app/Jobs/StatisticsReport.php
+++ app/Jobs/StatisticsReport.php
@@ -19,49 +19,51 @@ use App\Events\GenerateReport;
 class StatisticsReport implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
-    protected $reportMail;
     protected $reportFile;
     protected $dataHandler;
     protected $filename;
     protected $user;
-    protected $reportEvent;
+    protected $needTables = [];
     
     public function __construct(array $allowedTables, \App\User $user)
     {
         $this->user = $user;
         $this->filename = $this->generateFileName();
+        $this->reportFile = new ReportToXLSX();
         $this->dataHandler = new ReportableDataHandler($this->filename);
-        $this->reportFile = (new ReportToXLSX())->putTitle('Отчет')->putHeader(['Таблица', 'Количество записей']);
-        
         foreach ($allowedTables as $table) {
-            if (request()->input($table)) {
-                $this->addStatistics($table, app(Statistics::class)->getTableCount($table));
-            }
+            if (request()->input($table)) $this->needTables[] = $table;
         }
-        $this->reportEvent = (new GenerateReport($this->dataHandler))->forUser($user);
-        $this->reportMail = (new ReportMail($this->dataHandler))->withDefaultTemplate();
     }
     
     protected function generateFileName()
     {
         return $this->user->id . '_' . md5(microtime()) . '.xlsx';
     }
-        
+    
     protected function addStatistics($name, $data)
     {
         $this->dataHandler->addStatistics([
             'name' => trans("messages.tables.$name.name"),  'data' => $data
         ]);
-        $this->reportFile->putRowWithStandartStyle([trans("messages.tables.$name.name"), $data]);
+        $this->reportFile->putRowWithStandartStyle([
+            trans("messages.tables.$name.name"), $data
+        ]);
     }
  
     public function handle()
     {
-        if (! empty($this->reportMail->tables)) {
-            $this->reportFile->save($this->filename);
-            \Mail::to($this->user->email)->send($this->reportMail);
-            event($this->reportEvent);
-            ReportDelete::dispatch($this->filename)->onQueue('reports')->delay(now()->addHours(config('app.delayBeforeDeleteReportInHours')));
+        if (! empty($this->needTables)) {
+        $this->reportFile->putTitle('Отчет')->putHeader(['Таблица', 'Количество записей']);
+        foreach ($this->needTables as $table) {
+            $this->addStatistics($table, app(Statistics::class)->getTableCount($table));
+        }
+        $this->reportFile->save($this->filename);
+        $reportMail = (new ReportMail($this->dataHandler))->withDefaultTemplate();   
+        
+        \Mail::to($this->user->email)->send($reportMail);
+        event((new GenerateReport($this->dataHandler))->forUser($this->user));
+        ReportDelete::dispatch($this->filename)->onQueue('reports')->delay(now()->addHours(config('app.delayBeforeDeleteReportInHours')));
         }
     }
 }
