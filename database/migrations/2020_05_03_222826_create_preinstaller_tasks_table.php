955780c6fff769d5922d284e0c94c6c7e4ec0e61
Mikhail Peretyatko <mihanya@list.ru>
2020-05-18 00:16:51 +0300

    Updated preinstaller_tasks migration

--- database/migrations/2020_05_03_222826_create_preinstaller_tasks_table.php
+++ database/migrations/2020_05_03_222826_create_preinstaller_tasks_table.php
@@ -30,11 +30,11 @@ class CreatePreinstallerTasksTable extends Migration
         factory(PreinstallerTask::class)
             ->create(
                 [
-                    'name' => 'Собрания кредиторов',
+                    'name' => 'Собрание кредиторов',
                     'repeatability' => true,
                     'reference_interval_id' => 6,
                     'interval_value' => 1,
-                    'subtasks' => json_encode([
+                    'subtasks' => [
                         [
                             'description' => 'Подготовить уведомление о собрании кредиторов',
                             'showable_by' => 2,
@@ -145,7 +145,338 @@ class CreatePreinstallerTasksTable extends Migration
                             'score' => 2,
                             'not_delayable' => true,
                         ],
-                    ]),
+                    ],
+                ]
+            )
+        ;
+        
+        factory(PreinstallerTask::class)
+            ->create(
+                [
+                    'name' => 'Заседание комитета кредиторов',
+                    'repeatability' => true,
+                    'reference_interval_id' => 6,
+                    'interval_value' => 1,
+                    'subtasks' => [
+                        [
+                            'description' => 'Подготовить уведомление о заседании комитета кредиторов',
+                            'showable_by' => 2,
+                            'reference_interval_id' => 3,
+                            'delay' => -15,
+                            'reference_difficulty_id' => 1,
+                            'reference_priority_id' => 2,
+                            'score' => 1,
+                            'not_delayable' => true,
+                        ],
+                        [
+                            'description' => 'Опубликовать сообщение о заседании комитета кредиторов в ЕФРСБ',
+                            'showable_by' => 2,
+                            'reference_interval_id' => 3,
+                            'delay' => -15,
+                            'reference_difficulty_id' => 1,
+                            'reference_priority_id' => 3,
+                            'score' => 1,
+                            'not_delayable' => true,
+                        ],
+                        [
+                            'description' => 'Отправить уведомление о заседании комитета кредиторов',
+                            'showable_by' => 1,
+                            'reference_interval_id' => 3,
+                            'delay' => -15,
+                            'reference_difficulty_id' => 1,
+                            'reference_priority_id' => 3,
+                            'score' => 1,
+                            'not_delayable' => true,
+                        ],
+                        [
+                            'description' => 'Составление отчетов конкурсного управляющего и иных материалов по повестке',
+                            'showable_by' => 3,
+                            'reference_interval_id' => 3,
+                            'delay' => -10,
+                            'reference_difficulty_id' => 4,
+                            'reference_priority_id' => 3,
+                            'score' => 5,
+                            'not_delayable' => false,
+                        ],
+                        [
+                            'description' => 'Подготовка бюллетеней и журнала',
+                            'showable_by' => 1,
+                            'reference_interval_id' => 3,
+                            'delay' => -3,
+                            'reference_difficulty_id' => 1,
+                            'reference_priority_id' => 1,
+                            'score' => 1,
+                            'not_delayable' => false,
+                        ],
+                        [
+                            'description' => 'Отправить отчеты и иные необходимые материалы по заседанию комитета кредиторов в адрес СРО',
+                            'showable_by' => 1,
+                            'reference_interval_id' => 3,
+                            'delay' => -5,
+                            'reference_difficulty_id' => 1,
+                            'reference_priority_id' => 3,
+                            'score' => 1,
+                            'not_delayable' => true,
+                        ],
+                        [
+                            'description' => 'Участие в заседании комитета кредиторов',
+                            'showable_by' => 1,
+                            'reference_interval_id' => 3,
+                            'delay' => 0,
+                            'reference_difficulty_id' => 2,
+                            'reference_priority_id' => 3,
+                            'score' => 3,
+                            'not_delayable' => true,
+                        ],
+                        [
+                            'description' => 'Подготовка протокола заседания комитета кредиторов',
+                            'showable_by' => 1,
+                            'reference_interval_id' => 3,
+                            'delay' => 1,
+                            'reference_difficulty_id' => 2,
+                            'reference_priority_id' => 2,
+                            'score' => 3,
+                            'not_delayable' => false,
+                        ],
+                        [
+                            'description' => 'Отправка протокола заседания комитета кредиторов в СРО',
+                            'showable_by' => 1,
+                            'reference_interval_id' => 3,
+                            'delay' => 1,
+                            'reference_difficulty_id' => 1,
+                            'reference_priority_id' => 3,
+                            'score' => 1,
+                            'not_delayable' => true,
+                        ],
+                        [
+                            'description' => 'Отправка протокола заседания комитета кредиторов с материалами в Арбитражный суд',
+                            'showable_by' => 2,
+                            'reference_interval_id' => 3,
+                            'delay' => 2,
+                            'reference_difficulty_id' => 1,
+                            'reference_priority_id' => 3,
+                            'score' => 3,
+                            'not_delayable' => true,
+                        ],
+                        [
+                            'description' => 'Опубликование результатов заседания комитета кредиторов в ЕФРСБ',
+                            'showable_by' => 2,
+                            'reference_interval_id' => 3,
+                            'delay' => 2,
+                            'reference_difficulty_id' => 1,
+                            'reference_priority_id' => 3,
+                            'score' => 2,
+                            'not_delayable' => true,
+                        ],
+                    ],
+                ]
+            )
+        ;
+        
+        factory(PreinstallerTask::class)
+            ->create(
+                [
+                    'name' => 'Собрание работников должника',
+                    'repeatability' => true,
+                    'reference_interval_id' => 6,
+                    'interval_value' => 1,
+                    'subtasks' => [
+                        [
+                            'description' => 'Подготовить уведомление о собрании работников должника',
+                            'showable_by' => 2,
+                            'reference_interval_id' => 3,
+                            'delay' => -31,
+                            'reference_difficulty_id' => 1,
+                            'reference_priority_id' => 2,
+                            'score' => 1,
+                            'not_delayable' => true,
+                        ],
+                        [
+                            'description' => 'Подготовка бюллетеней',
+                            'showable_by' => 1,
+                            'reference_interval_id' => 3,
+                            'delay' => -30,
+                            'reference_difficulty_id' => 1,
+                            'reference_priority_id' => 1,
+                            'score' => 1,
+                            'not_delayable' => false,
+                        ],
+                        [
+                            'description' => 'Опубликовать сообщение о собрании работников должника в ЕФРСБ',
+                            'showable_by' => 2,
+                            'reference_interval_id' => 3,
+                            'delay' => -30,
+                            'reference_difficulty_id' => 1,
+                            'reference_priority_id' => 3,
+                            'score' => 1,
+                            'not_delayable' => true,
+                        ],
+                        [
+                            'description' => 'Отправить уведомление о собрании работников должника',
+                            'showable_by' => 1,
+                            'reference_interval_id' => 3,
+                            'delay' => -30,
+                            'reference_difficulty_id' => 1,
+                            'reference_priority_id' => 3,
+                            'score' => 1,
+                            'not_delayable' => true,
+                        ],
+                        [
+                            'description' => 'Подготовка протокола собрания работников должника',
+                            'showable_by' => 1,
+                            'reference_interval_id' => 3,
+                            'delay' => 1,
+                            'reference_difficulty_id' => 2,
+                            'reference_priority_id' => 2,
+                            'score' => 3,
+                            'not_delayable' => false,
+                        ],
+                        [
+                            'description' => 'Отправка протокола собрания работников должника с материалами в Арбитражный суд',
+                            'showable_by' => 2,
+                            'reference_interval_id' => 3,
+                            'delay' => 1,
+                            'reference_difficulty_id' => 1,
+                            'reference_priority_id' => 3,
+                            'score' => 3,
+                            'not_delayable' => true,
+                        ],
+                        [
+                            'description' => 'Опубликование результатов собрания работников должника в ЕФРСБ',
+                            'showable_by' => 2,
+                            'reference_interval_id' => 3,
+                            'delay' => 1,
+                            'reference_difficulty_id' => 1,
+                            'reference_priority_id' => 3,
+                            'score' => 2,
+                            'not_delayable' => true,
+                        ],
+                    ],
+                ]
+            )
+        ;
+        
+        factory(PreinstallerTask::class)
+            ->create(
+                [
+                    'name' => 'Ежеквартальная отчетность финансового управляющего',
+                    'repeatability' => true,
+                    'reference_interval_id' => 6,
+                    'interval_value' => 1,
+                    'subtasks' => [
+                        [
+                            'description' => 'Получить выписки банка и иные документы, необходимые для подготовки отчета финансового управляющего',
+                            'showable_by' => 3,
+                            'reference_interval_id' => 3,
+                            'delay' => -20,
+                            'reference_difficulty_id' => 2,
+                            'reference_priority_id' => 3,
+                            'score' => 3,
+                            'not_delayable' => false,
+                        ],
+                        [
+                            'description' => 'Составление отчета финансового управляющего',
+                            'showable_by' => 3,
+                            'reference_interval_id' => 3,
+                            'delay' => -7,
+                            'reference_difficulty_id' => 4,
+                            'reference_priority_id' => 3,
+                            'score' => 10,
+                            'not_delayable' => false,
+                        ],
+                        [
+                            'description' => 'Отправить отчет в адрес кредиторов',
+                            'showable_by' => 3,
+                            'reference_interval_id' => 3,
+                            'delay' => 0,
+                            'reference_difficulty_id' => 1,
+                            'reference_priority_id' => 3,
+                            'score' => 1,
+                            'not_delayable' => true,
+                        ],
+                        [
+                            'description' => 'Отправка отчета в СРО',
+                            'showable_by' => 2,
+                            'reference_interval_id' => 3,
+                            'delay' => 0,
+                            'reference_difficulty_id' => 1,
+                            'reference_priority_id' => 4,
+                            'score' => 1,
+                            'not_delayable' => true,
+                        ],
+                        [
+                            'description' => 'Отправка отчета в Арбитражный суд',
+                            'showable_by' => 2,
+                            'reference_interval_id' => 3,
+                            'delay' => 0,
+                            'reference_difficulty_id' => 1,
+                            'reference_priority_id' => 4,
+                            'score' => 1,
+                            'not_delayable' => true,
+                        ],
+                    ],
+                ]
+            )
+        ;
+        
+        factory(PreinstallerTask::class)
+            ->create(
+                [
+                    'name' => 'Судебный спор (в качестве истца)',
+                    'repeatability' => false,
+                    'subtasks' => [
+                        [
+                            'description' => 'Подготовить возражение на отзыв (иной документ) ответчика',
+                            'showable_by' => 5,
+                            'reference_interval_id' => 3,
+                            'delay' => -20,
+                            'reference_difficulty_id' => 3,
+                            'reference_priority_id' => 2,
+                            'score' => 10,
+                            'not_delayable' => false,
+                        ],
+                        [
+                            'description' => 'Участие в судебном заседании',
+                            'showable_by' => 1,
+                            'reference_interval_id' => 3,
+                            'delay' => 0,
+                            'reference_difficulty_id' => 4,
+                            'reference_priority_id' => 3,
+                            'score' => 10,
+                            'not_delayable' => false,
+                        ],
+                    ],
+                ]
+            )
+        ;
+        
+        factory(PreinstallerTask::class)
+            ->create(
+                [
+                    'name' => 'Судебный спор (в качестве ответчика)',
+                    'repeatability' => false,
+                    'subtasks' => [
+                        [
+                            'description' => 'Подготовить отзыв на исковое заявление (иной документ) истца',
+                            'showable_by' => 5,
+                            'reference_interval_id' => 3,
+                            'delay' => -30,
+                            'reference_difficulty_id' => 3,
+                            'reference_priority_id' => 2,
+                            'score' => 10,
+                            'not_delayable' => false,
+                        ],
+                        [
+                            'description' => 'Участие в судебном заседании',
+                            'showable_by' => 1,
+                            'reference_interval_id' => 3,
+                            'delay' => 0,
+                            'reference_difficulty_id' => 4,
+                            'reference_priority_id' => 3,
+                            'score' => 10,
+                            'not_delayable' => false,
+                        ],
+                    ],
                 ]
             )
         ;
